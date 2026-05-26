372b8038f8494076f78d4e14b07250e10c8d2478
tatnit2023-ops <tatnit2023@gmail.com>
2026-05-27 05:11:39 +0530

    updated the code.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # java_project
 this is a java project to be tested for jenkins
+
+HelloWorld
+==========
+
+A simple Java application that can be compiled into a .jar file using Maven. This is a update after the webhook is applied.
+
+To build
+--------
+    mvn clean package
+
+To run
+------
+    java -cp target/helloworld-1.1.jar com.coveros.demo.helloworld.HelloWorld
---
+++ src/main/java/com/coveros/demo/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.coveros.demo.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+    final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("h:mm:ss a 'on' MMMM d, yyyy'.'");
+    final LocalDateTime now = LocalDateTime.now();
+
+    System.out.println("Hello, This was a test made available using jenkins ! The current time is " + dtf.format(now));
+  }
+
+}
